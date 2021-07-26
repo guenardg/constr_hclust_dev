4e5d38cfccfb0736ded37802fea21c13ac20ceed
guenardg <guillaume.guenard@gmail.com>
2021-07-26 10:46:59 -0400

    Added the disjoint set case

--- Disjoint/Disjoint.R
+++ Disjoint/Disjoint.R
@@ -3,8 +3,72 @@
 ##
 ## rm(list=ls())
 ##
-library(spdep)
+## Pierre's example
+##
 library(constr.hclust)
+##
+## For simplicity of the example, matrix Y only contains one response variable
+var = c(1.5, 0.2, 5.1, 3.0, 2.1, 1.4)
+ex.Y = data.frame(var)
+# Site coordinates, matrix xy
+x.coo = c(-1, -2, -0.5, 0.5, 2, 1)
+y.coo = c(-2, -1, 0, 0, 1, 2)
+ex.xy = data.frame(x.coo, y.coo)
+# Matrix of connecting edges E
+from = c(1,1,2,3,4,3,4)
+to = c(2,3,3,4,5,6,6)
+ex.E = data.frame(from, to)
+##
+## Carry out constrained clustering analysis
+test.out <-
+  constr.hclust(
+    dist(ex.Y),       # Response dissimilarity matrix
+    method="ward.D2", # Clustering method
+    links=ex.E,       # File of link edges (constraint) E
+    coords=ex.xy      # File of geographic coordinates
+  )
+##
+## Plot the results for k = 3
+par(mfrow=c(1,2))
+plot(test.out, k=3)
+stats:::plot.hclust(test.out, hang=-1)
+##
+test.out$merge
+test.out$height
+test.out$order
+##
+from = c(1,1,2,4,4)
+to = c(2,3,3,5,6)
+ex.E2 = data.frame(from, to)
+##
+test.out2 <-
+  constr.hclust(
+    dist(ex.Y), # Response dissimilarity matrix
+    method="ward.D2", # Clustering method
+    links=ex.E2, # File of link edges (constraint) E
+    coords=ex.xy # File of geographic coordinates
+  )
+##
+par(mfrow=c(1,2))
+plot(test.out2, k=3) # Même graphique que test.out (OK)
+stats:::plot.hclust(test.out2, hang=-1)
+##
+str(test.out2)
+test.out2$merge
+test.out2$height
+test.out2$order
+##
+test.out_mod <- test.out2
+test.out_mod$height[5] <- NA
+test.out_mod$merge[5,] <- c(3,4)
+test.out_mod$order <- c(1,2,3,6,4,5)
+##
+par(mfrow=c(1,2))
+plot(test.out_mod, k=3) # Même graphique que test.out (OK)
+stats:::plot.hclust(test.out_mod, hang=-1)
+##
+##
+library(spdep)
 ##
 coords <- cbind(x=c(1,1,1,2,2,4,5,5,5,7,7,8),y=c(1,2,3,1,2,3,4,3,2,1,2,1))
 plot(coords,pch=LETTERS[1:nrow(coords)])
@@ -37,12 +101,20 @@ grpWD2cst_constr_hclust <-
     neighbors, coords)
 ## stats:::plot.hclust(grpWD2cst_constr_hclust, hang=-1)
 ##
-plot(grpWD2cst_constr_hclust, k=2, links=TRUE, las=1, xlab="Eastings",
+plot(grpWD2cst_constr_hclust, k=3, links=TRUE, las=1, xlab="Eastings",
      ylab="Northings", pch=21L, cex=3, lwd=3)
 ##
-grpWD2cst_constr_hclust$height
-grpWD2cst_constr_hclust$merge
-## cutree(grpWD2cst_constr_hclust,12)
+grpWD2cst_constr_hclust$height[10:11] <- NA
+grpWD2cst_constr_hclust$merge[10,] <- c(8,9)  ## Fusion acceptable par hclust.
+grpWD2cst_constr_hclust$merge[11,] <- c(7,10) ## Idem
+grpWD2cst_constr_hclust$order <- 1:length(grpWD2cst_constr_hclust$order)
+## stats:::plot.hclust(grpWD2cst_constr_hclust, hang=-1, ylim=c(0,1))
 ##
+grpWD2cst_constr_hclust2 <- grpWD2cst_constr_hclust
+grpWD2cst_constr_hclust2$height[-(10:11)] <- 0
+grpWD2cst_constr_hclust2$merge[-(10:11),] <- 0
+cutree(grpWD2cst_constr_hclust2,3)
+
+
 
 
